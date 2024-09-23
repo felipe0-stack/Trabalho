02a94d124125390a270eb5d79ecfbbfc5f25e696
felipe0-stack <felipeschwarz120@gmail.com>
2024-09-23 14:19:44 -0300

    Atualizando FormCadCategoria, TabelaCategoria e TelaCadastroCategoria

--- src/componentes/Telas/Tabelas/TabelaCategorias.jsx
+++ src/componentes/Telas/Tabelas/TabelaCategorias.jsx
@@ -1,13 +1,55 @@
 import { Button, Container, Table } from "react-bootstrap";
 
-export default function TabelaCategorias (props) {
+export default function TabelaCategorias(props) {
+
+    function excluirCategoria(categoria) {
+        if (window.confirm("Deseja realmente excluir a categoria " + categoria.descricao + "?")) {
+            props.setListaCategorias(props.listaCategorias.filter((item) => {
+                return item.codigo !== categoria.codigo; // Exclui a categoria pelo código
+            }));
+        }
+    }
+
+    function alterarCategoria(categoria) {
+        props.setModoAlterar(true);
+        props.setExibirTabela(false);
+        props.setCategoriaSelecionada({ ...categoria }); // Define a categoria a ser alterada
+    }
+
     return (
         <>
             <Container>
-                <Button className="mb-3" variant="primary">Adicionar</Button>
-                <Table>
-
+                <Button className="mb-3" variant="primary" onClick={() => { props.setExibirTabela(false); }}>
+                    Adicionar
+                </Button>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Código</th>
+                            <th>Descrição</th>
+                            <th>Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {props.listaCategorias?.map((categoria) => {
+                            return (
+                                <tr key={categoria.codigo}>
+                                    <td>{categoria.codigo}</td>
+                                    <td>{categoria.descricao}</td>
+                                    <td>
+                                        <Button onClick={() => { alterarCategoria(categoria) }} variant="warning">
+                                            Alterar
+                                        </Button>{' '}
+                                        <Button onClick={() => { excluirCategoria(categoria) }} variant="danger">
+                                            Excluir
+                                        </Button>
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
                 </Table>
+                <p>Qtd de categorias cadastradas: {props.listaCategorias.length}</p>
             </Container>
         </>
     );
--- src/componentes/Telas/TelaCadastroCategoria.jsx
+++ src/componentes/Telas/TelaCadastroCategoria.jsx
@@ -1,14 +1,41 @@
-import FormCadCategoria from "./Formularios/FormCadCategoria";
+import { useState } from "react";
 import Pagina from '../layouts/Pagina';
+import TabelaCategorias from './Tabelas/TabelaCategorias';
+import FormCadCategoria from './Formularios/FormCadCategoria'
 import { Alert } from "react-bootstrap";
 
-export default function TelaCadastroCategoria(props) {
+export default function TelaCadastroCategoria() {
+    const [exibirTabela, setExibirTabela] = useState(true);
+    const [listaDeCategorias, setListaDeCategorias] = useState([]);
+    const [modoAlterar, setModoAlterar] = useState(false);
+    const [categoriaSelecionada, setCategoriaSelecionada] = useState({
+        codigo: "",
+        descricao: ""
+    });
+
     return (
         <Pagina>
             <Alert className="text-center" variant="success">
-                <h2 className="text-center">Tela de cadastro de Categoria</h2>
+                <h2 className="text-center">Tela de Cadastro de Categorias</h2>
             </Alert>
-            <FormCadCategoria />
+            {exibirTabela ? 
+                <TabelaCategorias
+                    listaCategorias={listaDeCategorias}
+                    setListaCategorias={setListaDeCategorias}
+                    setExibirTabela={setExibirTabela}
+                    setModoAlterar={setModoAlterar}
+                    setCategoriaSelecionada={setCategoriaSelecionada}
+                /> :
+                <FormCadCategoria
+                    listaCategorias={listaDeCategorias}
+                    setListaCategorias={setListaDeCategorias}
+                    setExibirTabela={setExibirTabela}
+                    modoAlterar={modoAlterar}
+                    setModoAlterar={setModoAlterar}
+                    categoriaSelecionada={categoriaSelecionada}
+                    setCategoriaSelecionada={setCategoriaSelecionada}
+                />
+            }
         </Pagina>
     );
 }
